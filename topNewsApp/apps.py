369e44bbe5c8f0f7e084d7735200e00e1a6fe5f8
navneet <navneet03kmr@gmail.com>
2016-08-09 05:47:46 +0530

    ''

--- topNewsApp/apps.py
+++ topNewsApp/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class TopnewsappConfig(AppConfig):
-    name = 'topNewsapp'
+class TopnewsAppConfig(AppConfig):
+    name = 'topNewsApp'
